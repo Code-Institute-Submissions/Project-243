69e9b409d698d4e698debc76e264a980d80d903f
JoshRudge22 <>
2023-11-24 20:50:51 +0000

    Added an intro to the code and the random module, will change the text in print later on

--- run.py
+++ run.py
@@ -1,3 +1,10 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+
+print("Want to play?")
+play_game = input("Can you step up? (yes/no: ").lower()
+
+if play_game == 'yes':
+    print("Your time to shine!")
+
+else:
+    print("Bottle job")
