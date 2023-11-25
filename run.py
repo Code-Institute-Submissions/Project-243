bc5958e810cd3578ff38de8cf005995b0b37bc86
JoshRudge22 <>
2023-11-25 11:50:16 +0000

    added rounds and  a scoring system to the rest of the code, code runs up until it you get the option of what to enter. Will look into this later

--- run.py
+++ run.py
@@ -14,8 +14,6 @@ if play_game == 'yes':
     print("You will pick a number between 1-5, if you and the computer pick different numbers you score")
     print("If you and the computer match numbers then the computer makes the save!")
 
-
-
     player_goals = 0
     computer_saves = 0
 
@@ -23,7 +21,7 @@ if play_game == 'yes':
 def game():
     global player_goals, computer_saves
 
-    player_choice = input("Choose either '1', '2', '3', '4', '5'")
+    player_choice = input("Choose either '1', '2', '3', '4', '5': ")
 
     player_choice = int(player_choice)
 
@@ -34,10 +32,44 @@ def game():
 
     computer_choice = random.choice([1,2,3,4,5])
 
-    print(f + name + "goes {player_choice}, computer has gone {computer_choice}")
+    print(name + "goes {player_choice}, computer has gone {computer_choice}")
+
+    if player_choice == computer_choice:
+        computer_saves += 1
+        print("save")
+    else:
+        player_goals +=1
+        print("goal")
+    return player_goals, computer_saves
+
+def play_rounds(rounds):
+    player_total_goals = 0
+    computer_total_saves = 0
+
+    for _ in range(rounds):
+        player_goals, computer_saves = game()
+
+        player_total_goals += player_goals
+        computer_total_saves += computer_saves
+        print(f"Total goals scored: {player_total_goals}")
+        print(f"Total saves made: {computer_total_saves}")
 
 if __name__ == "__main__":
-    start_game()
+    player_goals = 0
+    computer_saves = 0
+    game()
+
+    for _ in range(5):
+        game()
+
+    if player_goals >= 3 or computer_saves >= 3:
+        print("That's all they needed!")
+        if player_goals >= 3:
+            print(name + "has won it for" + nationality)
+        else:
+            print("Computer wins")
+    else:
+        game()
 
 else:
     print("Okay")
