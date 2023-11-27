17b3ade303f2918a9b3452c8de489d28c020b2d6
JoshRudge22 <>
2023-11-27 10:08:01 +0000

    issues with entering the details and running the code has been fixed, only issue left is to get the player choice and computer choice to show once the user has picked a number

--- run.py
+++ run.py
@@ -17,59 +17,52 @@ if play_game == 'yes':
     player_goals = 0
     computer_saves = 0
 
+    def game():
+        global player_goals, computer_saves
 
-def game():
-    global player_goals, computer_saves
+        player_choice = input("Choose either '1', '2', '3', '4', '5': ")
 
-    player_choice = input("Choose either '1', '2', '3', '4', '5': ")
+        player_choice = int(player_choice)
 
-    player_choice = int(player_choice)
+        if player_choice not in [1,2,3,4,5]:
+            print("You need to enter either '1', '2', '3', '4', '5' in order to play!")
+            return game()
 
-    if player_choice not in [1,2,3,4,5]:
-        print("You need to enter either '1', '2', '3', '4', '5' in order to play!")
+        computer_choice = random.choice([1,2,3,4,5])
 
-    return game()
+        print(name + " goes {player_choice}, computer has gone {computer_choice}")
 
-    computer_choice = random.choice([1,2,3,4,5])
-
-    print(name + "goes {player_choice}, computer has gone {computer_choice}")
-
-    if player_choice == computer_choice:
-        computer_saves += 1
-        print("save")
-    else:
-        player_goals +=1
-        print("goal")
-    return player_goals, computer_saves
-
-def play_rounds(rounds):
-    player_total_goals = 0
-    computer_total_saves = 0
-
-    for _ in range(rounds):
-        player_goals, computer_saves = game()
-
-        player_total_goals += player_goals
-        computer_total_saves += computer_saves
-        print(f"Total goals scored: {player_total_goals}")
-        print(f"Total saves made: {computer_total_saves}")
-
-if __name__ == "__main__":
-    player_goals = 0
-    computer_saves = 0
-    game()
-
-    for _ in range(5):
-        game()
-
-    if player_goals >= 3 or computer_saves >= 3:
-        print("That's all they needed!")
-        if player_goals >= 3:
-            print(name + "has won it for" + nationality)
+        if player_choice == computer_choice:
+            computer_saves += 1
+            print("save")
         else:
-            print("Computer wins")
-    else:
-        game()
+            player_goals +=1
+            print("goal")
+        return player_goals, computer_saves
 
-else:
-    print("Okay")
+    def play_rounds(rounds):
+        player_total_goals = 0
+        computer_total_saves = 0
+
+        for _ in range(rounds):
+            player_goals, computer_saves = game()
+
+            player_total_goals += player_goals
+            computer_total_saves += computer_saves
+            print(f"Total goals scored: {player_total_goals}")
+            print(f"Total saves made: {computer_total_saves}")
+
+    if __name__ == "__main__":
+        for _ in range(5):
+            game()
+
+        if player_goals >= 3 or computer_saves >= 3:
+            print("That's all they needed!")
+            if player_goals >= 3:
+                print(name + " has won it for " + nationality)
+            else:
+                print("Computer wins")
+        else:
+            game()
+    else:
+        print("Okay")
